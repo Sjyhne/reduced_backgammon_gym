714708d6d2a7212dd1b1819d57578545b58c4bb3
Sander Jyhne <sandsjyhne@gmail.com>
2021-02-23 12:33:56 +0100

    Soon finished with the basic functionality, needs to generate actions very soon

--- environment/backgammon.py
+++ environment/backgammon.py
@@ -69,6 +69,23 @@ class Backgammon:
         # Returns True if set(spots) is a subset of set(players_home_positions)
         return set(set(spots)).issubset(self.players_home_positions[color])
 
+    # Check for whether the piece is the furthest piece away from bearing off
+    # This is relevant when the die shows 2, and the player tries to move
+    # the innermost of the two places in home position. (1, 1) -> (1, 0) | 1 OFF
+    def is_src_furthest_piece(self, color, action):
+        src, dst = action
+        player_positions = self.get_players_positions()[color]
+        if color == WHITE:
+            if src == min(player_positions):
+                return True
+            else:
+                return False
+        else:
+            if src == max(player_positions):
+                return True
+            else:
+                return False
+
     # color is the color of the current player, and action
     # is a tuple for the src and dest for the action (src, dest)
     def is_valid(self, color, action):
@@ -89,16 +106,65 @@ class Backgammon:
             # Then we have to check whether or not the player can
             # Bear off
             elif self.can_bear_off(color):
-                print("Tries to bear off")
+                # If the target is not 10, then check if the source piece
+                # Is the last piece of all pieces in home
+                if target > N_SPOTS - 1 or target < 0:
+                    if self.is_src_furthest_piece(color, action):
+                        return True
+                    else:
+                        return False
+                
                 return True
 
         return False
 
-    def get_oppponent_color(self, color):
+    def get_opponent_color(self, color):
         if color == WHITE:
             return BLACK
         else:
             return WHITE
+
+    def knock_out_piece(self, color, target):
+        # Adding piece to the bar
+        self.bar[color] += 1
+        # Removing piece from the target spot
+        self.board[target].update({"count": 0, "color": None})
+
+    def remove_piece_from_src(self, color, src):
+        src_piece_count = self.board[src]["count"]
+        # Remove a piece from the src location and remove color if there are none left
+        if src_piece_count == 1:
+            self.board[src].update({"count": src_piece_count - 1, "color": None})
+        # Remove a piece from the src location
+        else:
+            self.board[src].update({"count": src_piece_count - 1})
+    
+    def add_piece_to_dst(self, color, dst):
+        dst_piece_count = self.board[dst]["count"]
+        # Add piece to destination location, and update color if there are no pieces there
+        if dst_piece_count == 0:
+            self.board[dst].update({"count": dst_piece_count + 1, "color": color})
+        # Add piece to destination location
+        else:
+            self.board[dst].update({"count": dst_piece_count + 1})
+
+    def move_piece_from_src_to_dest(self, color, action):
+        src, dst = action
+        src_piece_count = self.board[src]["count"]
+        if src_piece_count < 1:
+            raise RuntimeError("Cannot move from spot because there are no pieces left")
+        
+        self.remove_piece_from_src(color, src)
+        self.add_piece_to_dst(color, dst)
+        
+    def move_piece_off(self, color, action):
+        src, _ = action
+
+        # Remove piece from src
+        self.remove_piece_from_src(color, src)
+        # Add piece to "OFF"
+        self.off[color] += 1
+
         
     def execute_action(self, color, action):
         src, target = action
@@ -106,12 +172,18 @@ class Backgammon:
         if self.is_valid(color, action):
             # Check if there is an opponent piece on the target
             if self.board[target]["color"] != color:
-                # TODO: Move the opponent piece to the bar and move
-                # Our own piece to the spot
+                # Adding the opponents piece to the bar and removing it from the target
+                self.knock_out_piece(self.get_opponent_color(color), target)
+                # Move the piece from the source to the target
+                self.move_piece_from_src_to_dest(color, action)
+            # If the player can bear off (all home) and the target is not in
+            # The normal range of spots, then move the player off
+            elif self.can_bear_off(color) and target not in range(N_SPOTS):
+                # TODO: Implement bear off moves, almost done
                 ...
             else:
-                # TODO: Move our own piece to the spot
-                ...
+                # Move the piece from the source to the target
+                self.move_piece_from_src_to_dest(color, action)
 
 
 bg = Backgammon()
